b7d849cc465f3e427fa0d2e7fec593ef51d3c69f
Johnny <mister.cheban@gmail.com>
2016-03-23 09:23:19 +0300

    complete second part of HW03

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -22,4 +23,14 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  userid INTEGER,
+  id SERIAL PRIMARY KEY,
+  description TEXT,
+  calories INTEGER,
+  datetime TIMESTAMP,
+  FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,39 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100000, 1, 'Breakfast', 500, '2015-05-30 10:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100000, 2, 'Lunch', 1000, '2015-05-30 13:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100000, 3, 'Dinner', 500, '2015-05-30 20:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100000, 4, 'Breakfast', 500, '2015-05-31 10:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100000, 5, 'Lunch', 1000, '2015-05-31 13:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100000, 6, 'Dinner', 510, '2015-05-31 20:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100001, 7, 'Admin Breakfast', 500, '2015-05-30 10:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100001, 8, 'Admin Lunch', 1000, '2015-05-30 13:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100001, 9, 'Admin Dinner', 500, '2015-05-30 20:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100001, 10, 'Admin Breakfast', 500, '2015-05-31 10:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100001, 11, 'Admin Lunch', 1000, '2015-05-31 13:00:00');
+
+INSERT INTO meals(userid, id, description, calories, datetime)
+VALUES (100001, 12, 'Admin Dinner', 510, '2015-05-31 20:00:00');
