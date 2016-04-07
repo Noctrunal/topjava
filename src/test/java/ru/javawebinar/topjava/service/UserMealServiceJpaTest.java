808664d2cea0317ca00a18ec1546ad8c4ec24aeb
Nocturnal <mister.cheban@gmail.com>
2016-04-07 04:55:08 +0400

    Optional fix

--- src/test/java/ru/javawebinar/topjava/service/UserMealServiceJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceJpaTest.java
@@ -1,8 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 
 @ActiveProfiles({Profiles.POSTGRES, Profiles.JPA})
 public class UserMealServiceJpaTest extends AbstractUserMealServiceTest {
+    @Override
+    @Test(expected = UnsupportedOperationException.class)
+    public void testFindWithOwner() throws Exception {
+        super.testFindWithOwner();
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/UserServiceJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceJpaTest.java
@@ -1,8 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 
 @ActiveProfiles({Profiles.POSTGRES, Profiles.JPA})
 public class UserServiceJpaTest extends AbstractUserServiceTest {
+    @Override
+    @Test(expected = UnsupportedOperationException.class)
+    public void testFindWithMeals() throws Exception {
+        super.testFindWithMeals();
+    }
 }
